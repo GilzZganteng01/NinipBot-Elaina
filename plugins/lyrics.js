e422cde87665582ca17cd2450294eace31c77399
Hanif Uabidur Rohman Syah <hanifurohman05@gmail.com>
2024-08-13 11:08:54 +0700

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -1,65 +1,42 @@
-import  axios from "axios"
-import cheerio from "cheerio"
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `*• Example:* ${usedPrefix + command} *[Name song]*`;
-  
+const handler = async (m, {
+  conn,
+  text,
+  usedPrefix,
+  command
+}) => {
+  if (!text) throw `Ex: ${usedPrefix}${command} Bawa dia kembali`
   try {
-    let lirik = await findSongs(text);
-    let caption = `*[ ${command === "lirik" ? command + " SEARCH" : command.toUpperCase().split("").join(" ")} ]*
-*• Title:* ${lirik.title}
-*• Album:* ${lirik.album}
+    const data = await (await fetch(`https://api.betabotz.eu.org/api/search/lirik?lirik=${text}&apikey=${lann}`)).json()
+    const caption = `
+${data.result.lyrics}
 
-\`\`\`${lirik.lyrics}\`\`\``;
-    conn.sendFile(m.chat, lirik.thumb, null, caption, m)
+ℹ️ More info:
+🔗 ${data.result.image}
+🎤 Artist: ${data.result.artist}`
+    await conn.relayMessage(m.chat, {
+      extendedTextMessage:{
+        text: caption, 
+        contextInfo: {
+          externalAdReply: {
+            title: `🎵 ${data.result.title} - ${data.result.artist} 🎵`,
+            mediaType: 1,
+            previewType: 0,
+            renderLargerThumbnail: true,
+            thumbnailUrl: data.result.image,
+            sourceUrl: ''
+          }
+        }, mentions: [m.sender]
+    }}, {})
   } catch (e) {
-    throw "Request Error";
+    console.log(e)
+    m.reply('Terjadi kesalahan, silahkan coba lagi nanti')
   }
 }
-handler.help = ["songfind", "lirik", "lyrics", "findsong"].map(a => a + " *[Name song]*");
-handler.tags = ["internet"];
-handler.command = ["songfind", "lirik", "lyrics", "findsong"];
 
-export default handler 
+handler.help = ['lirik'].map(v => v + ' <Title>')
+handler.tags = ['internet']
+handler.command = /^(lirik|lyrics|lyric)$/i
 
-
-async function findSongs(text) {
-  try {
-    const { data } = await axios.get(
-      "https://songsear.ch/q/" + encodeURIComponent(text),
-    );
-    const $ = cheerio.load(data);
-    const result = {
-      title:
-        $("div.results > div:nth-child(1) > .head > h3 > b").text() +
-        " - " +
-        $("div.results > div:nth-child(1) > .head > h2 > a").text(),
-      album: $("div.results > div:nth-child(1) > .head > p").text(),
-      number: $("div.results > div:nth-child(1) > .head > a")
-        .attr("href")
-        .split("/")[4],
-      thumb: $("div.results > div:nth-child(1) > .head > a > img").attr("src"),
-    };
-
-    const { data: lyricData } = await axios.get(
-      `https://songsear.ch/api/song/${result.number}?text_only=true`,
-    );
-    const lyrics = lyricData.song.text_html
-      .replace(/<br\/>/g, "\n")
-      .replace(/&#x27;/g, "'");
-
-    return {
-      status: true,
-      title: result.title,
-      album: result.album,
-      thumb: result.thumb,
-      lyrics: lyrics,
-    };
-  } catch (err) {
-    console.log(err);
-    return {
-      status: false,
-      error: "Unknown error occurred",
-    };
-  }
-}
+export default handler
