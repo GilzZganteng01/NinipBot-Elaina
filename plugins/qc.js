f20b50bd6bd763b0aa734dd70596c3caae54e83d
Hanif Uabidur Rohman Syah <hanifurohman05@gmail.com>
2024-08-13 07:52:49 +0700

    Update qc.js

--- plugins/qc.js
+++ plugins/qc.js
@@ -1,64 +1,97 @@
-import { sticker } from '../lib/sticker.js';
-import uploadFile from '../lib/uploadFile.js';
+import axios from 'axios'
+import { Sticker } from 'wa-sticker-formatter'
+import uploadImage from '../lib/uploadImage.js'
+let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
+    try {
+        let q = m.quoted ? m.quoted: m
+        let mime = (q.msg || q).mimetype || ''
+        let txt = text ? text: typeof q.text == 'string' ? q.text: ''
+        if (!txt) throw `Example: ${usedPrefix + command} halo`
+        let avatar = await conn.profilePictureUrl(q.sender, 'image').catch(_ => 'https://i.ibb.co/2WzLyGk/profile.jpg')
+        avatar = /tele/.test(avatar) ? avatar: await uploadImage((await conn.getFile(avatar)).data)
+        if (!/image\/(jpe?g|png)/.test(mime)) {
+            let req = await fakechat(txt, q.name, avatar)
+            let stiker = await createSticker(req, false, stickpack, stickauth)
+            conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+        } else {
+            let img = await q.download()
+            let url = await uploadImage(img)
+            let req = await fakechatImg(url, txt, q.name, avatar)
+            let stiker = await createSticker(req, false, stickpack, stickauth)
+            conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+        }
+    } catch (e) {
+        throw e
+    }
+}
+handler.help = ['qc']
+handler.tags = ['sticker']
+handler.command = /^(fc|fakechat|qc)$/i
+handler.limit = true
+handler.onlyprem = true
+export default handler
 
-const handler = async (m, { conn, text, args }) => {
-  var pp = 'https://i.ibb.co/2WzLyGk/profile.jpg'
-  if (!args[0] && !m.quoted)
-    return m.reply(`Please provide a text (Type or mention a message) !`)
-  if (m.quoted && !text) {
-    try {
-      userPfp = await conn.profilePictureUrl(m.sender, "image");
-    } catch (e) {
-      userPfp = pp;
+async function fakechat(text, name, url) {
+    let body = {
+        "type": "quote",
+        "format": "webp",
+        "backgroundColor": "#000000",
+        "width": 512,
+        "height": 512,
+        "scale": 2,
+        "messages": [{
+        "avatar": true,
+        "from": {
+            "first_name": name,
+            "language_code": "en",
+            "name": name,
+            "photo": {
+            "url": url
+            }
+        },
+        "text": text,
+        "replyMessage": {}
+        }]
     }
-  } else {
-    try {
-      userpp = await conn.profilePictureUrl(m.sender, "image");
-    } catch (e) {
-      userp = pp;
-    }
-  }
-  var trimtext = text.length > 50 ? text.substring(0, 50 - 3) + "..." : text,
-    trimqtext
-  if (m.quoted && m.quoted.text) {
-    trimqtext = m.quoted.text.length > 50 ? m.quoted.text.substring(0, 50 - 3) + "..." : m.quoted.text
-  }
-  var q = m.quoted ? m.quoted : m
-  var mime = (q.msg || q).mimetype || q.mediaType || ''
-  var media, img
-  if (/image/.test(mime)) {
-    img = await q.download?.()
-    if (img) media = await uploadFile(img)
-  }
-  var tkw = !trimtext && m.quoted && m.quoted.text ? trimqtext : trimtext
-  var qwe = trimtext && m.quoted && m.quoted.text ? {
-    qname: m.quoted.name,
-    qtext: trimqtext
-  } : {}
-  try {
-    var json = await axios.get(API('xzn', 'api/qc', {
-      text: tkw,
-      username: !trimtext && m.quoted ? m.quoted.name : m.name,
-      avatar: await uploadFile(await getbuffer(userPfp)),
-      ...(media ? {
-        "media": media
-      } : {}),
-      ...qwe
-    }, 'apikey'), {
-      responseType: "arraybuffer"
-    })
-    var stiker = await sticker(json.data, global.packname, global.author)
-    if (stiker) return conn.sendFile(m.chat, stiker, 'Quotly.webp', '', m)
-  } catch (e) {
-    log({
-      e
-    })
-    return e.toString()
-  }
+    let res = await axios.post('https://btzqc.betabotz.eu.org/generate', body);
+    return Buffer.from(res.data.result.image, "base64");
 }
 
-handler.help = ['quotly']
-handler.tags = ['sticker']
-handler.command = /^(qc|quoted|quotly)$/i
+async function fakechatImg(url, text, name, avatar) {
+    let body = {
+        "type": "quote",
+        "format": "png",
+        "backgroundColor": "#000000",
+        "width": 512,
+        "height": 768,
+        "scale": 2,
+        "messages": [{
+        "entities": [],
+        "media": {
+            "url": url
+        },
+        "avatar": true,
+        "from": {
+            "id": 1,
+            "name": name,
+            "photo": {
+            "url": avatar
+            }
+        },
+        "text": text,
+        "replyMessage": {}
+        }]
+    }
+    let res = await axios.post('https://btzqc.betabotz.eu.org/generate', body);
+    return Buffer.from(res.data.result.image, "base64");
+}
 
-export default handler
+async function createSticker(req, url, packName, authorName, quality) {
+    let stickerMetadata = {
+        type: 'full',
+        pack: stickpack,
+        author: stickauth,
+        quality
+    }
+return (new Sticker(req ? req: url, stickerMetadata)).toBuffer()
+}
