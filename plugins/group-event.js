5fd0aaa608e66d94ffd46f9cefb7e8c0ea9ded07
Hanif Uabidur Rohman Syah <hanifurohman05@gmail.com>
2024-08-15 11:43:49 +0700

    Update group-event.js

--- plugins/group-event.js
+++ plugins/group-event.js
@@ -4,13 +4,6 @@ import { generateWAMessageFromContent } from '@adiwajshing/baileys';
 export async function handler(m, { text, args, usedPrefix, command }) {
   if (!args[0]) return m.reply(`Enter a list of events, Example: ${usedPrefix + command} Ada Event Ges`);
   let [evtName, evtDesc, evtLocation, evtLink] = text.split(',');
-  let today = new Date();
-  let tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1); // tambahkan 1 hari
-  tomorrow.setHours(today.getHours()); // atur jam yang sama
-  tomorrow.setMinutes(today.getMinutes()); // atur menit yang sama
-  tomorrow.setSeconds(today.getSeconds()); // atur detik yang sama
-
   let msg = generateWAMessageFromContent(m.chat, {
     messageContextInfo: {
       messageSecret: randomBytes(32)
@@ -25,7 +18,7 @@ export async function handler(m, { text, args, usedPrefix, command }) {
         name: evtLocation || 'Indonesia'
       },
       joinLink: evtLink || 'https://s.id/',
-      startTime: tomorrow
+      startTime: new Date() * 86400000
     }
   }, {});
 
@@ -36,7 +29,7 @@ export async function handler(m, { text, args, usedPrefix, command }) {
 
 handler.help = ["eventgc"].map((a) => a + " *[question]*");
 handler.tags = ["group"];
-handler.command = ["eventgc"];
+handler.command = ["eventgc","event"];
 handler.group = true;
 
 export default handler;
