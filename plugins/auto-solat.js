50b7a72117bc1160ba74248ce2af62cf2c390142
Hanif Uabidur Rohman Syah <hanifurohman05@gmail.com>
2024-08-20 15:49:49 +0700

    Update auto-solat.js

--- plugins/auto-solat.js
+++ plugins/auto-solat.js
@@ -1,4 +1,3 @@
-
 //import fetch from "node-fetch"
 export async function before(m) {
     this.autosholat = this.autosholat ? this.autosholat : {}
@@ -11,13 +10,13 @@ export async function before(m) {
     //let jadwalSholat = data.data.timings;
     let jadwalSholat = {
         Imsyak: "04:18",
-        Subuh: "04:28",
-        Terbit: "05:43",
-        Dhuha: "06:07",
-        Dzuhur: "11:43",
-        Ashar: "15:04",
-        Maghrib: "17:40",
-        Isha: "18:51",
+        Subuh: "04:28-05:42",
+        Terbit: "05:43-06:06",
+        Dhuha: "06:07-11:42",
+        Dzuhur: "11:43-15:03",
+        Ashar: "15:04-17:39",
+        Maghrib: "17:40-18:50",
+        Isha: "18:51-19:00",
     }
     const date = new Date((new Date).toLocaleString("en-US", {
         timeZone: "Asia/Jakarta"
@@ -26,21 +25,22 @@ export async function before(m) {
     const minutes = date.getMinutes();
     const timeNow = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
     for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
-        if (timeNow === waktu) {
+        const [start, end] = waktu.split("-");
+        if (timeNow >= start && timeNow <= end) {
             let caption;
             if (sholat === 'Imsyak') {
                 caption = `Hai kak @${who.split`@`[0]},\nWaktu *${sholat} telah tiba, segera minum dan selesaikan niat untuk berpuasa (bagi yang berpuasa) 🙂\n\n*${waktu}*\n_untuk wilayah Yogyakarta dan sekitarnya._`
             } else if (sholat === 'Terbit') {
                 caption = `Hai kak @${who.split`@`[0]},\nWaktu *Terbit* telah tiba, mari kita berdoa dan memulai hari dengan penuh semangat! 🌟\n\n*${waktu}*\n_untuk wilayah Yogyakarta dan sekitarnya._`
             } else {
-                caption = `Hai kak @${who.split`@`[0]},\nWaktu *${sholat}* telah tiba, ambilah air wudhu dan segeralah shalat 🙂.\n\n*${waktu}*\n_untuk wilayah Yogyakarta dan sekitarnya._`
+                caption = `Hai kak @${who.split`@`[0]}, Sudah Sholat Belum?\nWaktu *${sholat}* telah tiba, ambilah air wudhu dan segeralah shalat 🙂.\n\n*${waktu}*\n_untuk wilayah Yogyakarta dan sekitarnya._`
             }
             this.autosholat[id] = [
                 this.reply(m.chat, caption, null, {
-    contextInfo: {
-      mentionedJid: [who]
-    }
-  }),
+                    contextInfo: {
+                        mentionedJid: [who]
+                    }
+                }),
                 setTimeout(() => {
                     delete this.autosholat[id]
                 }, 57000)
