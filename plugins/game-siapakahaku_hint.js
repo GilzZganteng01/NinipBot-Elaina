099b7272af5b345144e500a0a028d9764eacfb4d
Hanif Uabidur Rohman Syah <hanifurohman05@gmail.com>
2024-08-23 12:26:40 +0700

    y

--- plugins/game-siapakahaku_hint.js
+++ plugins/game-siapakahaku_hint.js
@@ -1,13 +1,15 @@
-let handler = async (m, { conn }) => {
-    conn.siapakahaku = conn.siapakahaku ? conn.siapakahaku : {}
-    let id = m.chat
-    if (!(id in conn.siapakahaku)) throw false
-    let json = conn.siapakahaku[id][1]
-    let ans = json.jawaban
+const handler = async (m, { conn }) => {
+    conn.siapakahaku = conn.siapakahaku || {};
+    const id = m.chat;
+    if (!(id in conn.siapakahaku)) throw false;
+    const json = conn.siapakahaku[id][1];
+    const ans = json.jawaban;
     // kalau ini error clue nya ak mau ada tanda (_) nya ganti string dalam function di bawah ini jadi huruf kecil
-    let clue = ans.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
-    m.reply('```' + clue + '```')
-}
-handler.command = /^maka/i
-handler.limit = true
-module.exports = handler
+    const clue = ans.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_');
+    m.reply('```' + clue + '```');
+  };
+  
+  handler.command = /^maka/i;
+  handler.limit = true;
+  
+  export default handler;
