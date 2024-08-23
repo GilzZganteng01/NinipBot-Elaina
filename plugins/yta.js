a21a9c9401ebe8e1b35efcd91d68395a9b00bf2d
Hanif Uabidur Rohman Syah <hanifurohman05@gmail.com>
2024-08-23 13:51:01 +0700

    Fix

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,15 +1,25 @@
-let handler = async (m, {conn, text, usedPrefix}) => {
-  if (!text) throw 'Berikan URL dari YouTube!'
-  try {   
-    var aud = `https://aemt.me/youtube?url=${text}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg` 
-    await conn.sendMessage(m.chat, { audio: { url: aud }, mimetype: 'audio/mpeg' }, { quoted: m })    
-  } catch (e) {
-    throw 'Video/Audio Tidak Ditemukan'
+import { youtube } from 'btch-downloader';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `*Example:* ${usedPrefix} ${command} https://www.youtube.com/watch?v=Z28dtg_QmFw`;
+
+conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
+  try {
+    const data = await youtube(text);
+    await conn.sendMessage(m.chat, {
+      audio: { url: data.mp3 },
+      mimetype: 'audio/mpeg'
+    }, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-}
-handler.command = handler.help = ['yta'];
+};
+
+handler.help = handler.command = ['ytmp3', 'yta'];
 handler.tags = ['downloader'];
 handler.exp = 0;
 handler.limit = true;
 handler.premium = false;
+
 export default handler;
