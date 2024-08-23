b64903a1f235206891538e812a476100ce470f30
Hanif Uabidur Rohman Syah <hanifurohman05@gmail.com>
2024-08-23 13:20:46 +0700

    ya

--- plugins/game-family100.js
+++ plugins/game-family100.js
@@ -1,48 +1,32 @@
-import fs from 'fs';
-import fetch from 'node-fetch';
-
-const winScore = 500;
+import { family100 } from '@bochilteam/scraper'
+const winScore = 4999
 
 async function handler(m) {
-  this.game = this.game ? this.game : {};
-  const id = 'family100_' + m.chat;
-
-  if (id in this.game) {
-    if (this.game[id].id !== undefined) {
-      return this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini' + '\nKetik *Nyerah* untuk mengakhiri / *tunggu 5 detik*', this.game[id].msg);
+    this.game = this.game ? this.game : {}
+    let id = 'family100_' + m.chat
+    if (id in this.game) {
+        this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', this.game[id].msg)
+        throw false
     }
-    delete this.game[id];
-    throw false;
-  }
-
-  this.game[id] = {};
-  const src = await (await fetch(`https://api.betabotz.eu.org/api/game/family100-2?apikey=${lann}`)).json();
-  const json = src[Math.floor(Math.random() * src.length)];
-
-  const caption = `
- ┌─⊷ *SOAL*
-▢ *Soal:* ${json.soal}
-▢ Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
-▢ (beberapa jawaban terdapat spasi)
-▢ ketik *MENYERAH* tanpa replay soal jika menyerah
-▢ replay soal saat akan *MENJAWAB*
-└──────────────
+    const json = await family100()
+    let caption = `
+*Soal:* ${json.soal}
+Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
+(beberapa jawaban terdapat spasi)
 `: ''}
-+${winScore} XP! tiap jawaban benar
-`.trim();
-
-  this.game[id] = {
-    id,
-    msg: await m.reply(caption),
-    ...json,
-    terjawab: Array.from(json.jawaban, () => false),
-    winScore,
-  };
++${winScore} XP tiap jawaban benar
+    `.trim()
+    this.game[id] = {
+        id,
+        msg: await this.reply(m.chat, caption, m),
+        ...json,
+        terjawab: Array.from(json.jawaban, () => false),
+        winScore,
+    }
 }
 
-handler.help = ['family100'];
-handler.tags = ['game'];
-handler.group = true;
-handler.command = /^family100$/i;
+handler.help = ['family100']
+handler.tags = ['game']
+handler.command = /^family100$/i
 
-export default handler;
+export default handler
