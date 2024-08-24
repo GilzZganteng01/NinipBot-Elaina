2e0597df833b0b46d5819dd28a7d4a284e9f8c70
Hanif Uabidur Rohman Syah <hanifurohman05@gmail.com>
2024-08-24 07:44:06 +0700

    gtw

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,8 +1,5 @@
-export default async function handler(m) {
-  return handlerBefore(m);
-}
-
-async function handlerBefore(m) {
+let handler = m => m
+handler.before = m => {
   let user = global.db.data.users[m.sender]
   if (user.afk > -1) {
     m.reply(`
@@ -28,9 +25,11 @@ Selama ${clockString(new Date - afkTime)}
   return true
 }
 
-export function clockString(ms) {
+export default handler
+
+function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
+		}
