150eedf21b422e9d0934c5816fb60d1d4fc2104e
Hanif Uabidur Rohman Syah <hanifurohman05@gmail.com>
2024-08-26 22:45:46 +0700

    fix bail

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,7 +2,7 @@ var handler = async (m, { text }) => {
   let user = global.db.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  conn.sendMessage(`
+  m.reply(`
 ${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
 `)
 }
